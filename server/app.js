c929b655c74ff351af33166b1fa33ac77480cda3
deejay94 <dahjoyner12@gmail.com>
2018-01-03 13:02:19 -0500

    Initial Commit
    -Created sever and model folder and app.js file
    -Installed packages
    -required packages in app.js
    -Add middleware and CORS support
    -connect to database and server on port 3000
    
    Next step is to create the routes

---
+++ server/app.js
@@ -0,0 +1,29 @@
+const express = require('express')
+const bodyParser = require('body-parser')
+const mongoose = require('mongoose')
+const methodOverride = require('method-override')
+const _ = require('lodash')
+
+//create app
+const app = express()
+
+// add middleware
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.json());
+app.use(methodOverride('X-HTTP-Method-Override'));
+
+// CORS Support
+app.use(function(req, res, next) {
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
+  res.header('Access-Control-Allow-Headers', 'Content-Type');
+  next();
+});
+
+// Connect to MongoDB
+mongoose.connect('mongodb://localhost/youdance');
+mongoose.connection.once('open', function() {
+
+  console.log('Listening on port 3000...');
+  app.listen(3000);
+});
