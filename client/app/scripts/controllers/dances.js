89e4c02c21874de9c7f4124e93ba7a250c25e1a9
deejay94 <dahjoyner12@gmail.com>
2018-01-03 15:56:33 -0500

    Hardcode dance data into controller
    Hardcode dance data into controller so that data shows up on the page

--- client/app/scripts/controllers/dances.js
+++ client/app/scripts/controllers/dances.js
@@ -8,10 +8,27 @@
  * Controller of the clientApp
  */
 angular.module('clientApp')
-  .controller('DancesCtrl', function () {
-    this.awesomeThings = [
-      'HTML5 Boilerplate',
-      'AngularJS',
-      'Karma'
+  .controller('DancesCtrl', function ($scope) {
+    $scope.dances = [
+      {
+          name: 'whip/nae nae',
+          genre: 'hip-hop',
+          url: 'https://www.youtube.com/watch?v=vjW8wmF5VWc'
+        },
+        {
+            name: 'milly rock',
+            genre: 'hip-hop',
+            url: 'https://www.youtube.com/watch?v=PMzDoFuVgRg'
+          },
+          {
+              name: 'billy bounce',
+              genre: 'hip-hop',
+              url: 'https://www.youtube.com/watch?v=zRW8LJ2_984'
+          },
+          {
+              name: 'Do it like me',
+              genre: 'hip-hop',
+              url: 'https://www.youtube.com/watch?v=7hl0aTooA90'
+          }
     ];
   });
