7e4b98cd5227c1a968883339020c23883eb683c6
deejay94 <dahjoyner12@gmail.com>
2018-01-03 19:01:18 -0500

    Generate files using yo angular for future use

---
+++ client/app/scripts/controllers/dance-add.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name clientApp.controller:DanceAddCtrl
+ * @description
+ * # DanceAddCtrl
+ * Controller of the clientApp
+ */
+angular.module('clientApp')
+  .controller('DanceAddCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
---
+++ client/app/scripts/controllers/dance-edit.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name clientApp.controller:DanceEditCtrl
+ * @description
+ * # DanceEditCtrl
+ * Controller of the clientApp
+ */
+angular.module('clientApp')
+  .controller('DanceEditCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
