59372e5a3a0b9ee9f02a6c395c8d94bfc4465a53
deejay94 <dahjoyner12@gmail.com>
2018-01-05 11:33:06 -0500

    Add success and failure messages
    Add a message for div with id of 'message' to display for success and
    failures

--- client/app/scripts/controllers/dance-add.js
+++ client/app/scripts/controllers/dance-add.js
@@ -10,9 +10,16 @@
 angular.module('clientApp')
   .controller('DanceAddCtrl', ['$scope', '$http', '$location', '$routeParams', function ($scope, $http, $location, $routeParams) {
     $scope.addDance = function(){
-  		$http.post('http://localhost:3000/dances', $scope.dance).then(function(response){
+  		$http.post('http://localhost:3000/dances', $scope.dance)
+      .then(function(response){
   			window.location.href='#!/dances';
-  		});
+  		})
+      .then(function(){
+        $('#message').text('Successfully Added a dance')
+  		})
+      .catch(function() {
+        $('#message').text('Please fill in all necessary fields')
+      })
   	}
 
   }]);
